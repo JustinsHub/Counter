effd55666b5053f2f0946f86722b80a5065bf8ca
Justin Z <contactjustinzamora@gmail.com>
2021-01-19 11:33:59 -0800

    added Counter component and useState

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,15 @@
-
+import React, {useState} from 'react'
 
 const Counter = ()=>{
+    const [count, setCount] = useState(0)
     return (
-        <>
-        <b>'Hello World'</b>
-        </>
+    <>
+    <b>Counter: {count}</b>
+    <div>
+    <button onClick={() => {setCount(count + 1)}}>Add</button>
+    <button onClick={() => {setCount(count - 1)}}>Subtract</button>
+    </div>
+    </>
     )
 }
 
